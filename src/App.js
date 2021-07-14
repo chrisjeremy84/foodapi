84e903e450fda6b75af7dad7248ff12d6a191c99
5thfader <chrisjeremy84@live.com>
2021-07-14 21:19:09 +0200

    Illustrate data with images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Axios from 'axios'
+import { useState } from 'react'
 
 function App() {
+  const [query, setquery] = useState("")
+  const [recipes, setRecipes] = useState([])
+
+  const APP_ID = 'e987e8e0'
+  const APP_KEY = '35a9e696b72c09519d52837697974efc'
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  
+
+  const getData = async() => {
+      const result = await Axios.get(url);
+      setRecipes(result.data.hits)
+      console.log(result)
+      setquery("")
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    getData()
+  }
+
+  const onChange = (e) => {
+    setquery(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1 onClick={getData}>Food Searching App</h1>
+   <form className="search-form" onSubmit={submitHandler}>
+    <input type="text" 
+    placeholder="search food" 
+    autoComplete="off" 
+    onChange={onChange}
+    value={query}
+    />
+    <button type="submit">Search</button>
+   </form>
+
+   <div className="recipes">
+     {recipes !== [] && recipes.map(recipe =><h2>{recipe.recipe.label}</h2>)}
+     </div>
     </div>
   );
 }
